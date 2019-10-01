14b9e4d539bde8c184327d1c1e97bc6118fa4559
milimolo <bondemand@hotmail.dk>
2019-10-01 10:27:21 +0200

    Start

---
+++ CompulsoryMovieRating/CompulsoryMovieRating/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompulsoryMovieRating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

f85179cb9f6df40a445c6321ec6214023c3c0e7e
milimolo <bondemand@hotmail.dk>
2019-10-03 11:32:48 +0200

    Made 1-6

--- CompulsoryMovieRating/CompulsoryMovieRating/Movie.cs
+++ CompulsoryMovieRating/CompulsoryMovieRating/Movie.cs
@@ -6,17 +6,128 @@ using System.Text;
 
 namespace CompulsoryMovieRating
 {
-    class Movie
+    public class Movie
     {
+        Review review = new Review();
+
         public List<Review> reviews;
-        public Movie()
+
+        public void  loadData(string fileName)
         {
-            using (StreamReader r = new StreamReader("C:/Users/bonde/GitHub/Compulsory-Movie-Rating/CompulsoryMovieRating/TempRatings.json"))
+            using (StreamReader r = new StreamReader(fileName))
             {
                 string json = r.ReadToEnd();
                 reviews = JsonConvert.DeserializeObject<List<Review>>(json);
             }
         }
+
+        public int GetReviewsFromReviewer(int reviewerId)
+        {
+            int count = 0;
+            foreach (var review in reviews)
+            {
+                if (review.Reviewer == reviewerId)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int GetAverageGradeFromReviewer(int reviewerId)
+        {
+            int count = 0;
+            int totalGrade = 0;
+            foreach (var review in reviews)
+            {
+                if (review.Reviewer == reviewerId)
+                {
+                    count++;
+                    totalGrade = totalGrade + review.Grade;
+                }
+            }
+            if (totalGrade == 0)
+            {
+                throw new Exception("This reviewer has not graded any movies.");
+            }
+            int averageGrade = (totalGrade / count);
+            return averageGrade;
+        }
+
+        public int getReviewersExactGrade(int reviewerId, int grade)
+        {
+            int numberOfGrades = 0;
+
+            foreach (var review in reviews)
+            {
+                if (review.Reviewer == reviewerId && review.Grade == grade)
+                {
+                    numberOfGrades++;
+                }
+            }
+            return numberOfGrades;
+
+        }
+
+        public int getNumberOfReviewsByMovie(int movieId)
+        {
+            int numberOfReviews = 0;
+
+            foreach (var review in reviews)
+            {
+                if (review.Movie == movieId)
+                {
+                    numberOfReviews++;
+                }
+            }
+            return numberOfReviews;
+
+        }
+        public int getAverageMovieRating(int movieId)
+        {
+            int count = 0;
+            int totalRating = 0;
+            foreach (var review in reviews)
+            {
+                if (review.Movie == movieId)
+                {
+                    count++;
+                    totalRating = totalRating + review.Grade;
+                }
+            }
+            if (totalRating == 0)
+            {
+                throw new Exception("This Movie has no ratings");
+            }
+            int averageRating = (totalRating / count);
+            return averageRating;
+        }
+
+        public int getSpecificRatingOfMovie(int movieId, int grade)
+        {
+            int numberOfRatings = 0;
+
+            foreach (var review in reviews)
+            {
+                if (review.Movie == movieId && review.Grade == grade)
+                {
+                    numberOfRatings++;
+                }
+            }
+            return numberOfRatings;
+        }
+
+        /*
+        public int getMoviesWithHighestReview()
+        {
+            List<int>
+
+            foreach (var review in reviews)
+            {
+                
+            }
+        }
+        */
     }
 
     public class Review
--- CompulsoryMovieRating/MovieRatingTests/UnitTest1.cs
+++ CompulsoryMovieRating/MovieRatingTests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using CompulsoryMovieRating;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace MovieRatingTests
 {
@@ -6,9 +8,94 @@ namespace MovieRatingTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestNumberOfReviews()
         {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test1_1.json");
+            Assert.AreEqual(4, movie.GetReviewsFromReviewer(1));
+        }
 
+        [TestMethod]
+        public void Test1IsNegative()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test1_1.json");
+            Assert.IsFalse(movie.GetReviewsFromReviewer(1) < 0);
+        }
+
+        [TestMethod]
+        public void TestAverageReviews()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test2_1.json");
+            Assert.AreEqual(4, movie.GetAverageGradeFromReviewer(1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "This reviewer has not graded any movies.")]
+        public void TestAverageException()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test2_1.json");
+            movie.GetAverageGradeFromReviewer(4);
+        }
+
+        [TestMethod]
+        public void TestReviewersExactGrade()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test3_1.json");
+            Assert.AreEqual(3, movie.getReviewersExactGrade(2, 4));
+        }
+
+        [TestMethod]
+        public void TestNumberOfReviewsByMovie()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test4_1.json");
+            Assert.AreEqual(3, movie.getNumberOfReviewsByMovie(1488844));
+        }
+
+        [TestMethod]
+        public void TestAverageMovieRating()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test5_1.json");
+            Assert.AreEqual(4, movie.getAverageMovieRating(1488844));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "This Movie has no ratings")]
+        public void TestAverageMovieRatingException()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test5_1.json");
+            movie.getAverageMovieRating(885014);
+            
+        }
+
+        [TestMethod]
+        public void TestSpecificRatingOfMovie()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test6_1.json");
+            Assert.AreEqual(3, movie.getSpecificRatingOfMovie(1488844, 3));
+        }
+
+        [TestMethod]
+        public void TestMoviesWithHighestReview()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test7_1.json");
+            Assert.AreEqual(44937, movie.getMoviesWithHighestReview());
+        }
+
+        [TestMethod]
+        public void TestMoviesWithHighestReview2()
+        {
+            Movie movie = new Movie();
+            movie.loadData("TestJsons/Test7_1.json");
+            Assert.AreEqual(712664, movie.getMoviesWithHighestReview());
         }
     }
 }
